bf5b04b6ddda8d3fb8249b20c9445f220998872e
pi <malph@live.de>
2026-02-15 13:16:57 +0100

    remove debugging stuff from UnityMain.cs in build

--- Assets/Scripts/Game/Main/UnityMain.cs
+++ Assets/Scripts/Game/Main/UnityMain.cs
@@ -1,15 +1,13 @@
-using System;
 using DLS.Graphics;
 using DLS.Simulation;
 using Seb.Helpers;
-using Seb.Vis;
-using Seb.Vis.UI;
 using UnityEngine;
 
 namespace DLS.Game
 {
 	public class UnityMain : MonoBehaviour
 	{
+#if DEBUG
 		[Header("Dev Settings (editor only)")]
 		public bool openSaveDirectory;
 		public bool openInMainMenu;
@@ -18,110 +16,43 @@ namespace DLS.Game
 		public bool openA = true;
 		public string chipToOpenA;
 		public string chipToOpenB;
-
-		[Header("Temp test vars")] public Vector2 testVecA;
-		public Vector2 testVecB;
-		public Vector2 testVecC;
-		public Vector2 testVecD;
-		public Color testColA;
-		public Color testColB;
-		public Color testColC;
-		public Color testColD;
-		public ButtonTheme testButtonTheme;
-		public bool testbool;
-		public Anchor testAnchor;
-
-
-		public string testString;
-		public string testString2;
-		public uint testUint;
-		public uint testUint2;
-		public bool removeZeros;
-		public ushort testUshort;
-
-		[Header("Audio test")]
-		[Range(0,255)]public int noteIndex;
-		public int numNoteDivisions;
-		public double noteFreq;
-		public double refNoteFreq;
-		public bool useRef;
-		public int refIndex;
-		public float perceptualGain;
-		
-		public AudioState.WaveType waveType;
-
-		public bool restart;
-		public float speed = 1;
-		public float staccatoDelay;
-		public int waveIts = 20;
-		public bool songTestMode;
-		public NoteTest[] notes;
-		
-		// References
-		public static UnityMain instance;
-		AudioUnity audioUnity;
+#endif
 
 		void Awake()
 		{
-			instance = this;
-			audioUnity = FindFirstObjectByType<AudioUnity>();
+#if DEBUG
 			ResetStatics();
+#endif
 
 			AudioState audioState = new();
-			audioUnity.audioState = audioState;
+			FindFirstObjectByType<AudioUnity>().audioState = audioState;
 
 			Main.Init(audioState);
 
 
-			if (openInMainMenu || !Application.isEditor) Main.LoadMainMenu();
+#if DEBUG
+			if (openInMainMenu) Main.LoadMainMenu();
 			else Main.CreateOrLoadProject(testProjectName, openA ? chipToOpenA : chipToOpenB);
-
+#else
+			Main.LoadMainMenu();
+#endif
 		}
 
 		void Update()
 		{
-			noteFreq = SimAudio.CalculateFrequency(noteIndex / (double)numNoteDivisions);
-			refNoteFreq = SimAudio.CalculateFrequency(refIndex / (double)numNoteDivisions);
-			if (Application.isEditor) EditorDebugUpdate();
+#if DEBUG
+			EditorDebugUpdate();
+#endif
 
-			if (songTestMode) SongTest();
-			else Main.Update();
+			Main.Update();
 		}
 
-		void SongTest()
+		void OnDestroy()
 		{
-			/*
-			audioUnity.audioState.waveIterations = waveIts;
-			audioUnity.audioState.waveType = waveType;
-			audioUnity.audioState.InitFrame();
-
-			if (restart)
-			{
-				restart = false;
-				time = -0.2f;
-			}
-
-			float playT = 0;
-			time += Time.deltaTime * speed;
-			int i = 0;
-			foreach (NoteTest n in notes)
-			{
-				float startTime = playT + n.delay + staccatoDelay;
-				float endTime = startTime + n.duration;
-				if (time > startTime && time < endTime)
-				{
-					audioUnity.audioState.RegisterNote(n.noteIndex, n.isSharp, 15);
-					Debug.Log(i + ": " + n.noteIndex + (n.isSharp ? " Sharp " : "Natural") + $"  Freq = {audioUnity.audioState.GetFrequency(n.noteIndex, n.isSharp):0.00}");
-				}
-
-				i++;
-				playT = endTime;
-			}
-
-			audioUnity.audioState.NotifyAllNotesRegistered();
-			*/
+			if (Project.ActiveProject != null) Project.ActiveProject.NotifyExit();
 		}
 
+#if DEBUG
 		void EditorDebugUpdate()
 		{
 			if (InputHelper.AltIsHeld && InputHelper.IsKeyDownThisFrame(KeyCode.P))
@@ -152,11 +83,6 @@ namespace DLS.Game
 			}
 		}
 
-		void OnDestroy()
-		{
-			if (Project.ActiveProject != null) Project.ActiveProject.NotifyExit();
-		}
-
 		void OnValidate()
 		{
 			if (openSaveDirectory)
@@ -175,14 +101,6 @@ namespace DLS.Game
 			CameraController.Reset();
 			WorldDrawer.Reset();
 		}
-
-		[Serializable]
-		public struct NoteTest
-		{
-			public int noteIndex;
-			public bool isSharp;
-			public float delay;
-			public float duration;
-		}
+#endif
 	}
 }
